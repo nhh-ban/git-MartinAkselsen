84d4ad058a131dde555932561eae43a703f58c46
Martin Akselsen <martin.akselsen@gmail.student.nhh.no>
2023-10-05 14:52:51 +0200

    Task 4

--- Assignment5.R
+++ Assignment5.R
@@ -57,6 +57,67 @@ galaxies <- read.csv("clean_data.csv")
 # Task 3 
 # ----
 
+# I choose a_26 to be the main variable to investigate, because this is a 
+# variable that tells us the size of each galaxy
+
 galaxies %>% 
+  ggplot(aes(x = a_26)) +
+  geom_histogram(bins = 70, color = "white") +
+  theme_bw()
+
+
+
+# Task 4
+# ---- 
+
+raw_file4 <- readLines(con = "https://www.sao.ru/lv/lvgdb/article/UCNG_Table4.txt")
+
+# Identify the line number J of the separator line between the column names and
+# the rest of the data table 
+substr(x = raw_file4, start = 1, stop = 3) # Find that it is line 2
+
+# Create J as a symbol for line 2
+
+J <- (substr(x = raw_file4, start = 1, stop = 3) == "---") %>% 
+  which() %>% 
+  min()
+
+# Save the variable names 
+variable_names_task4 <- 
+  str_split(string = raw_file4[(J-1)], pattern = "\\|") %>% 
+  unlist() %>% 
+  str_trim
   
-  
+# Read the data into a csv file without the lines
+
+csv_task4 <- 
+  raw_file4[(J+1):764] %>% 
+  gsub("\\|", ",", .) %>% 
+  gsub(" ", "", .)
+
+csv_task4_names <- 
+  c(paste(variable_names_task4, collapse = ","),
+    csv_task4)
+
+
+cat(csv_task4_names, sep = "\n", file = "task4_clean.csv")
+
+# Read the data
+UCNG <- read_csv("task4_clean.csv")
+
+# Combine the two tables into one dataframe
+combined_df <- left_join(galaxies, UCNG, by = "name")
+
+# Plot the velocity and distance in a scatterplot
+
+combined_df %>% 
+  ggplot(aes(x = cz, y = D)) +
+  geom_point() +
+  geom_smooth() +
+  ylab("Distance from us") +
+  xlab("Velocity") +
+  theme_bw()
+
+# It looks like his observations were right. The galaxies with higher velocity
+# are the ones that are furthest away from us
+
